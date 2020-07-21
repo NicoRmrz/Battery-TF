420a3a8ed295c323aa41df9ca34ecf5e00f9e408
NicoRmrz <nicoramirez72@yahoo.com>
2020-07-21 14:05:50 -0700

    Add files via upload

--- openApp.sh
+++ openApp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #TO RUN at Start up edit ... sudo nano /etc/xdg/lxsession/LXDE-pi/autostart 
 cd /
-cd /home/pi/Desktop/Battery-TF/
+cd /home/pi/Desktop/Battery-TF-master/
 python3 Battery_TF_Main.py
 cd /
